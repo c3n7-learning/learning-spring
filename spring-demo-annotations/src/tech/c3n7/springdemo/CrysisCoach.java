955bb44d75bd1ed24d3b918472a8f15ff19ba0f4
Timothy Karani <kartimothy@gmail.com>
2022-03-26 20:46:48 +0300

    Annotating using properties file

--- spring-demo-annotations/src/tech/c3n7/springdemo/CrysisCoach.java
+++ spring-demo-annotations/src/tech/c3n7/springdemo/CrysisCoach.java
@@ -1,16 +1,28 @@
 package tech.c3n7.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CrysisCoach implements Coach{
+    @Value("${foo.name}")
+    private String name;
+    @Value("${foo.team}")
+    private String team;
+
+    @Autowired
+    @Qualifier("cutThroatFortuneService")
+    private FortuneService fortuneService;
+
     @Override
     public String getDailyWorkout() {
-        return "Buy another GPU";
+        return String.format("Buy another GPU, Says %s from team %s", name, team);
     }
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return fortuneService.getFortune();
     }
 }
