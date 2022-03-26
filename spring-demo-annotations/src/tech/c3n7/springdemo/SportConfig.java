e5fcd6ee79227f693fb3c6c44d0691e8fbf20341
Timothy Karani <kartimothy@gmail.com>
2022-03-26 21:39:20 +0300

    Using java code configuration

---
+++ spring-demo-annotations/src/tech/c3n7/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package tech.c3n7.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("tech.c3n7.springdemo")
+public class SportConfig {
+}
