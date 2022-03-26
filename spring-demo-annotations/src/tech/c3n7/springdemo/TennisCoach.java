f9cec15011c79187bce37aba481199d5ffe7c0eb
Timothy Karani <kartimothy@gmail.com>
2022-03-26 21:21:33 +0300

    Bean lifecycle methods

--- spring-demo-annotations/src/tech/c3n7/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/tech/c3n7/springdemo/TennisCoach.java
@@ -2,11 +2,12 @@ package tech.c3n7.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
     @Autowired
     @Qualifier("randomFortuneService")
@@ -19,6 +20,16 @@ public class TennisCoach implements Coach {
 
     public TennisCoach() {
         System.out.println(">> Inside default constructor");
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: doMyStartupStuff");
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: doMyCleanupStuff");
     }
 
 //    @Autowired
