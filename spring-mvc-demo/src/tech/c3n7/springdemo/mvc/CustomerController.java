ca7c28b62e1fbd925ec437aed6c44772efbf0875
Timothy Karani <kartimothy@gmail.com>
2022-03-27 22:09:18 +0300

    Investigating error type

--- spring-mvc-demo/src/tech/c3n7/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/tech/c3n7/springdemo/mvc/CustomerController.java
@@ -32,6 +32,9 @@ public class CustomerController {
     @RequestMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 
+        System.out.println("Last Name: -" + theCustomer.getLastName() + "-");
+//        System.out.println("Binding Result: " + theBindingResult);
+//        System.out.println("\n\n\n");
         if(theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
