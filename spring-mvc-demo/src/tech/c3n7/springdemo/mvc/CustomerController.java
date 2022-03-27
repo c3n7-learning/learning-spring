5aac6dde8b939e28010f1a9de8ab019571b7b7cf
Timothy Karani <kartimothy@gmail.com>
2022-03-27 20:55:34 +0300

    Doing base validation

--- spring-mvc-demo/src/tech/c3n7/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/tech/c3n7/springdemo/mvc/CustomerController.java
@@ -22,6 +22,7 @@ public class CustomerController {
     @RequestMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 
+        System.out.println("Last Name: -" + theCustomer.getLastName() + "-");
         if(theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
