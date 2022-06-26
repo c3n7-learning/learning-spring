f5cd6684ce990e3d680979c0a88563c5c4ab15cc
Timothy Karani <kartimothy@gmail.com>
2022-06-26 08:49:30 +0300

    First of many

---
+++ hb-04-eager-vs-lazy-demo/src/tech/c3n7/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,44 @@
+package tech.c3n7.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import tech.c3n7.hibernate.demo.entity.Course;
+import tech.c3n7.hibernate.demo.entity.Instructor;
+import tech.c3n7.hibernate.demo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            //start a transaction
+            session.beginTransaction();
+
+            // get the instructor from the db
+            int theId = 1;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            System.out.println("Instructor " + tempInstructor);
+
+            System.out.println("Courses " + tempInstructor.getCourses());
+
+            // commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Done.");
+        } finally {
+            // Clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
