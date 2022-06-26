ff908806929002f65bc2c27c4ee050b1a71bc29f
Timothy Karani <kartimothy@gmail.com>
2022-06-26 16:37:54 +0300

    Added dao

---
+++ web-customer-tracker/src/tech/c3n7/springdemo/dao/CustomerClassImpl.java
@@ -0,0 +1,33 @@
+package tech.c3n7.springdemo.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.c3n7.springdemo.entity.Customer;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class CustomerClassImpl implements CustomerDAO {
+
+    // Need to inject the hibernate session factory
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // create a query
+        Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+
+        // get the list of customers from the query (by executing it)
+        List<Customer> customers = theQuery.getResultList();
+
+        // return the list of retrieved customers
+        return customers;
+    }
+}
