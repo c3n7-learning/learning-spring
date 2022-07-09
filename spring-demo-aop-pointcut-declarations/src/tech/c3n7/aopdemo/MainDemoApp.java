8b8d364234e71a9780e1f6827519bcaa8c7a1589
Timothy Karani <kartimothy@gmail.com>
2022-07-09 15:26:38 +0300

    Declared pointcut expression

---
+++ spring-demo-aop-pointcut-declarations/src/tech/c3n7/aopdemo/MainDemoApp.java
@@ -0,0 +1,29 @@
+package tech.c3n7.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tech.c3n7.aopdemo.dao.AccountDAO;
+import tech.c3n7.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+    public static void main(String[] args) {
+
+        // read spring configuration java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from the spring container
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+        // call the business method
+        Account myAccount = new Account();
+        theAccountDAO.addAccount(myAccount, true);
+        theMembershipDAO.addAccount();
+        theMembershipDAO.addSillyMember();
+
+        theAccountDAO.doWork();
+        theMembershipDAO.goToSleep();
+
+        // close the context
+        context.close();
+    }
+}
---
+++ spring-demo-aop-pointcut-declarations/src/tech/c3n7/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,17 @@
+package tech.c3n7.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+import tech.c3n7.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount(Account theAccount, boolean vip) {
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+    }
+
+    public boolean doWork() {
+        System.out.println(getClass() + ": doWork");
+        return false;
+    }
+}
