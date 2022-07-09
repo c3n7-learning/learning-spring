8e4967bc708494752344cad403c7ef30b4d5bfb8
Timothy Karani <kartimothy@gmail.com>
2022-07-09 15:29:43 +0300

    Reusing pointcut expression

--- spring-demo-aop-pointcut-declarations/src/tech/c3n7/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/tech/c3n7/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,10 +11,16 @@ public class MyDemoLoggingAspect {
 
     // this is where we add all of our related advices for logging
     @Pointcut("execution(* tech.c3n7.aopdemo.dao.*.*(..))")
-    private void forDaoPackage(){}
+    private void forDaoPackage() {
+    }
 
     @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n==========>>> Executing @Before advice on method()");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n==========>>> Performing API Analytics");
     }
 }
