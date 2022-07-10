1ecb43add415efc51c7902e06c2643b904a7911e
Timothy Karani <kartimothy@gmail.com>
2022-07-10 06:50:04 +0300

    Created simple project

---
+++ mycoolapp/src/main/java/tech/c3n7/App.java
@@ -0,0 +1,13 @@
+package tech.c3n7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of Maven!" );
+    }
+}
