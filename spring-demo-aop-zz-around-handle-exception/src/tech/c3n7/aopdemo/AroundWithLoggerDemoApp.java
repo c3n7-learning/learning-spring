9825d0cd7b65b728fd4b1ef4878d56f23b321a7b
Timothy Karani <kartimothy@gmail.com>
2022-07-10 04:59:59 +0300

    first of many

---
+++ spring-demo-aop-zz-around-handle-exception/src/tech/c3n7/aopdemo/AroundWithLoggerDemoApp.java
@@ -0,0 +1,32 @@
+package tech.c3n7.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tech.c3n7.aopdemo.service.TrafficFortuneService;
+
+import java.util.logging.Logger;
+
+public class AroundWithLoggerDemoApp {
+    private static Logger myLogger =
+            Logger.getLogger(AroundWithLoggerDemoApp.class.getName());
+
+    public static void main(String[] args) {
+
+        // read spring configuration java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from the spring container
+        TrafficFortuneService theFortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
+
+        myLogger.info("\nMain Program: AroundDemoApp");
+
+        myLogger.info("Calling getFortune");
+
+        String data = theFortuneService.getFortune();
+        myLogger.info("\nMy fortune is: " + data);
+
+        myLogger.info("Finished");
+
+        // close the context
+        context.close();
+    }
+}
