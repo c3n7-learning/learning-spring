b529710665a8096da1fb4a7f9bf3a7a0b1cd0e81
Timothy Karani <kartimothy@gmail.com>
2022-07-12 02:22:10 +0300

    Can get one student

--- spring-rest-demo/src/main/java/tech/c3n7/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/tech/c3n7/springdemo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package tech.c3n7.springdemo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.c3n7.springdemo.entity.Student;
@@ -26,5 +27,11 @@ public class StudentRestController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return students;
+    }
+
+    // define endpoint for "/student/{studentId}"
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
