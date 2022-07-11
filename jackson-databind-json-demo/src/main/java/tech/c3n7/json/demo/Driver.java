cae1a8e34816602fc8832a95a2dd585477715254
Timothy Karani <kartimothy@gmail.com>
2022-07-12 00:48:20 +0300

    Can read full sample json

--- jackson-databind-json-demo/src/main/java/tech/c3n7/json/demo/Driver.java
+++ jackson-databind-json-demo/src/main/java/tech/c3n7/json/demo/Driver.java
@@ -12,7 +12,7 @@ public class Driver {
             ObjectMapper mapper = new ObjectMapper();
 
             // read JSON file and map/convert to Java POJO: data/sample-lite.json
-            Student student = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+            Student student = mapper.readValue(new File("data/sample-full.json"), Student.class);
 
             // print out first name and last name
             System.out.println("First name: " + student.getFirstName());
