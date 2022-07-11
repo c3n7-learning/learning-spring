b51e1d98eddfda63ea8b5e1721e0a8a3f7863e62
Timothy Karani <kartimothy@gmail.com>
2022-07-12 00:41:47 +0300

    Can read lite

---
+++ jackson-databind-json-demo/src/main/java/tech/c3n7/json/demo/Driver.java
@@ -0,0 +1,24 @@
+package tech.c3n7.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        try {
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // read JSON file and map/convert to Java POJO: data/sample-lite.json
+            Student student = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+            // print out first name and last name
+            System.out.println("First name: " + student.getFirstName());
+            System.out.println("Last name: " + student.getLastName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
