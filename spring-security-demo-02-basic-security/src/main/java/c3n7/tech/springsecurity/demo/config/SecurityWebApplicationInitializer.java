7555a6f127f474a905503c5114abe82247575d0d
Timothy Karani <kartimothy@gmail.com>
2022-07-11 09:45:41 +0300

    Can auth

---
+++ spring-security-demo-02-basic-security/src/main/java/c3n7/tech/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package c3n7.tech.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
