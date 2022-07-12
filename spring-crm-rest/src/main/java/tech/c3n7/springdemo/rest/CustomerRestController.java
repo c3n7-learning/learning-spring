0e61faa8bc972a4d18799219afbb5ba2f000c9fd
Timothy Karani <kartimothy@gmail.com>
2022-07-12 20:53:58 +0300

    Can delete

--- spring-crm-rest/src/main/java/tech/c3n7/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/tech/c3n7/springdemo/rest/CustomerRestController.java
@@ -47,4 +47,17 @@ public class CustomerRestController {
 
         return theCustomer;
     }
+
+    @DeleteMapping("/customer/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId) {
+        Customer customer = customerService.getCustomer(customerId);
+
+        if(customer == null) {
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        }
+
+        customerService.deleteCustomer(customerId);
+
+        return "Deleted customer id - " + customerId;
+    }
 }
