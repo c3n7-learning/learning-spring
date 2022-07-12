04b0062ee6487a944f5052b5b798f1b8e604cd97
Timothy Karani <kartimothy@gmail.com>
2022-07-12 21:37:26 +0300

    Hello world

---
+++ mycoolapp/src/main/java/tech/c3n7/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package tech.c3n7.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is  " + LocalDateTime.now();
+    }
+}
