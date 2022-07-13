2ae5c5b5ba20bac24e13a428a3a3051716509050
Timothy Karani <kartimothy@gmail.com>
2022-07-13 08:53:44 +0300

    done with hibernate crud dao implementation

--- cruddemo/src/main/java/tech/c3n7/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/tech/c3n7/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,9 +1,7 @@
 package tech.c3n7.springboot.cruddemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.c3n7.springboot.cruddemo.dao.EmployeeDAO;
 import tech.c3n7.springboot.cruddemo.entity.Employee;
 import tech.c3n7.springboot.cruddemo.service.EmployeeService;
@@ -26,5 +24,43 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
 
+        if(employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee with id " + employeeId;
+    }
 }
