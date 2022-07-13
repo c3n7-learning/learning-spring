edbe1f60aa443ee9fcd4fc6f171a5591de498449
Timothy Karani <kartimothy@gmail.com>
2022-07-13 07:28:52 +0300

    First of many

---
+++ cruddemo/src/test/java/tech/c3n7/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.c3n7.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
