9eb0754e7d856ab281352a485ca0bb5cf6ea93f5
Timothy Karani <kartimothy@gmail.com>
2022-07-13 06:57:53 +0300

    Can run from cmd

---
+++ 04-commandline-demo/src/test/java/tech/c3n7/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.c3n7.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
