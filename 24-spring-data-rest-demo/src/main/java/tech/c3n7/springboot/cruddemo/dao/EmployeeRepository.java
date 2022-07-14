d2797f54012c600f81d46274f76f19aa25f316ec
Timothy Karani <kartimothy@gmail.com>
2022-07-14 08:19:07 +0300

    Rename

--- 24-spring-data-rest-demo/src/main/java/tech/c3n7/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-demo/src/main/java/tech/c3n7/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package tech.c3n7.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.c3n7.springboot.cruddemo.entity.Employee;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
