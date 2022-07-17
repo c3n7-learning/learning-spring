e1c82ca5276d732ac61c3cccab2cf5e7175c296d
Timothy Karani <kartimothy@gmail.com>
2022-07-17 14:55:19 +0300

    Can fetch employees from database

--- 34-thymeleafdemo-employees-list-db/src/main/java/tech/c3n7/thymeleafdemo/controller/EmployeeController.java
+++ 34-thymeleafdemo-employees-list-db/src/main/java/tech/c3n7/thymeleafdemo/controller/EmployeeController.java
@@ -1,40 +1,31 @@
 package tech.c3n7.thymeleafdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tech.c3n7.thymeleafdemo.entity.Employee;
+import tech.c3n7.thymeleafdemo.service.EmployeeService;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-    // load employee data
-    private List<Employee> employees;
 
-    @PostConstruct
-    private void loadData() {
-        // create employees
-        Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@gmail.com");
-        Employee emp2 = new Employee(2, "John", "Adams", "adams@gmail.com");
-        Employee emp3 = new Employee(3, "Leary", "Washington", "leary@gmail.com");
+    private EmployeeService employeeService;
 
-        // create the list
-        employees= new ArrayList<>();
-
-        // add to list
-        employees.add(emp1);
-        employees.add(emp2);
-        employees.add(emp3);
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     // add mapping for "/list"
     @GetMapping("/list")
     public String listEmployees(Model theModel) {
+        List<Employee> employees = employeeService.findAll();
+
         theModel.addAttribute("employees", employees);
 
         return "list-employees";
