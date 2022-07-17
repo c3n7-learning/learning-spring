496b58ba3b971f97ee699e625b3d95a8a0cd8950
Timothy Karani <kartimothy@gmail.com>
2022-07-17 15:43:06 +0300

    Can update an employee

---
+++ 36-thymeleafdemo-employees-list-update/src/main/java/tech/c3n7/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package tech.c3n7.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.c3n7.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
