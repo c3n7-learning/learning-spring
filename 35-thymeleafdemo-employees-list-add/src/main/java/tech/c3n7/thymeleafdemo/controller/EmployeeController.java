08c53861b59358d93b66dba37647096812299da9
Timothy Karani <kartimothy@gmail.com>
2022-07-17 15:26:05 +0300

    Can save

--- 35-thymeleafdemo-employees-list-add/src/main/java/tech/c3n7/thymeleafdemo/controller/EmployeeController.java
+++ 35-thymeleafdemo-employees-list-add/src/main/java/tech/c3n7/thymeleafdemo/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tech.c3n7.thymeleafdemo.entity.Employee;
 import tech.c3n7.thymeleafdemo.service.EmployeeService;
@@ -29,5 +31,20 @@ public class EmployeeController {
         theModel.addAttribute("employees", employees);
 
         return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        Employee employee = new Employee();
+        theModel.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
     }
 }
