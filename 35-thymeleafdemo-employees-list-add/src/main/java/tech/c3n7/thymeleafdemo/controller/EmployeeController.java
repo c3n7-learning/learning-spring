376d89522bea3ccebe5f68e77a25dd4aa921183a
Timothy Karani <kartimothy@gmail.com>
2022-07-17 15:11:24 +0300

    First of many

---
+++ 35-thymeleafdemo-employees-list-add/src/main/java/tech/c3n7/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package tech.c3n7.thymeleafdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tech.c3n7.thymeleafdemo.entity.Employee;
+import tech.c3n7.thymeleafdemo.service.EmployeeService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        List<Employee> employees = employeeService.findAll();
+
+        theModel.addAttribute("employees", employees);
+
+        return "employees/list-employees";
+    }
+}
---
+++ 35-thymeleafdemo-employees-list-add/src/main/java/tech/c3n7/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package tech.c3n7.thymeleafdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.c3n7.thymeleafdemo.dao.EmployeeRepository;
+import tech.c3n7.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee employee = null;
+
+        if(result.isPresent()) {
+            employee = result.get();
+        } else {
+            throw new RuntimeException("Did not find employee id - "  + theId);
+        }
+
+        return employee;
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+}
